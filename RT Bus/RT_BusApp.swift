84d78090a69897a4b5ae80113fff395b85db8ed0
Aapo Laakso <aapolaakso@me.com>
2025-12-28 11:02:09 +0200

    Initial Commit

---
+++ RT Bus/RT_BusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RT_BusApp.swift
+//  RT Bus
+//
+//  Created by Aapo Laakso on 28.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct RT_BusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
